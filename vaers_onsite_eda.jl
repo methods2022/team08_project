90274f1830078b5b07d0a92a60951a3efaf4a424
Ziyin-Li <71303933+Ziyin-Li@users.noreply.github.com>
2022-05-16 18:01:18 -0400

    Update vaers_onsite_eda.jl

--- vaers_onsite_eda.jl
+++ vaers_onsite_eda.jl
@@ -2,8 +2,8 @@
 using CSV
 using DataFrames
 
-# get grouped counts and percentages for sex:
-function onsite_grouped_percentages_sex()
+# get grouped counts and percentages based on sex:
+function onsite_grouped_percentages()
     # read the onsite processed output csv into dataframe
     data = "onsite_processed_data_output.csv"
     df = DataFrame(CSV.File(data))
@@ -25,31 +25,5 @@ function onsite_grouped_percentages_sex()
     CSV.write("onsite_grouped_percentages_output.csv", grouped_counts)
 end
 
-onsite_grouped_percentages_sex()
-
-# get grouped counts and percentages for vaccine brand:
-function onsite_grouped_percentages_vax()
-    # read the onsite processed output csv into dataframe
-    data = "onsite_processed_data_output.csv"
-    df = DataFrame(CSV.File(data))
-    # drop rows with unknown sex info (SEX is 'U')
-    df = filter(:SEX => n -> (n==("F") || n==("M")), df)
-    # restrict the onsite interval to no greater than 2 days
-    df = filter(:NUMDAYS => n -> (n<=2), df)
-    # calculate counts for each vax-nd-symptom group
-    vax_nd_symp_counts = combine(groupby(df, [:VAX_MANU, :NUMDAYS, :SYMPTOM]), nrow => :count)
-    # calculate counts for each vax-nd group
-    vax_nd_counts = combine(groupby(df, [:VAX_MANU, :NUMDAYS]), nrow => :sum_count)
-    # merge the two dataframes together (left join)
-    grouped_counts = leftjoin(vax_nd_symp_counts, vax_nd_counts, on = [:VAX_MANU, :NUMDAYS])
-    # calculate percentages of symptoms for each vax-nd-symptom group
-    grouped_counts[!, :percentage] = grouped_counts.count ./ grouped_counts.sum_count
-    # write dataframes to csv files
-    CSV.write("onsite_grouped_counts_vax.csv", vax_nd_symp_counts)
-    CSV.write("onsite_sum_counts_by_symptom_vax.csv", vax_nd_counts)
-    CSV.write("onsite_grouped_percentages_vax.csv", grouped_counts)
-end
-
-onsite_grouped_percentages_vax()
-
+onsite_grouped_percentages()
 
